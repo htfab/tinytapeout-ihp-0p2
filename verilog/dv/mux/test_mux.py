22d806b50edf9e1c4dfbc5b75c8b9a6635d23bb7
Uri Shaked <uri@urishaked.com>
2025-03-30 12:25:38 +0300

    test(rom): test sequential ROM reading

--- verilog/dv/mux/test_mux.py
+++ verilog/dv/mux/test_mux.py
@@ -1,7 +1,7 @@
 import cocotb
 from cocotb.types import LogicArray
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles, Timer
+from cocotb.triggers import ClockCycles
 import re
 import os
 import binascii
@@ -9,19 +9,23 @@ import binascii
 
 async def enable_design(dut, mux_addr):
     dut._log.info(f"enabling design at mux address {mux_addr}")
-    dut.ctrl_ena.value = 0
-    dut.ctrl_sel_rst_n.value = 0
+
+    # reset the controller
     dut.ctrl_sel_inc.value = 0
-    await Timer(100, units="ns")
+    dut.ctrl_sel_rst_n.value = 0
+    dut.ctrl_ena.value = 0
+    await ClockCycles(dut.clk, 5)
     dut.ctrl_sel_rst_n.value = 1
-    await Timer(100, units="ns")
-    for i in range(mux_addr):
+    await ClockCycles(dut.clk, 5)
+
+    # send the number of pulses required
+    for c in range(mux_addr):
         dut.ctrl_sel_inc.value = 1
-        await Timer(100, units="ns")
+        await ClockCycles(dut.clk, 1)
         dut.ctrl_sel_inc.value = 0
-        await Timer(100, units="ns")
+        await ClockCycles(dut.clk, 1)
+
     dut.ctrl_ena.value = 1
-    await Timer(100, units="ns")
 
 
 @cocotb.test()
@@ -42,23 +46,23 @@ async def test_factory_test(dut):
 
     dut._log.info("test loopback")
     for i in range(256):
-        dut.uio_in.value = i
+        dut.uio_in.value = i ^ 0x55
         await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i
+        assert dut.uo_out.value == i ^ 0x55
 
     # with bit 0 of ui_in set to 1, module will enable bidirectional outputs and put a counter on both output and bidirectional output
-    dut.uio_in.value = LogicArray("Z" * 8)
     dut.ui_in.value = 0b1
 
     # reset it
     dut.rst_n.value = 0
+    dut.uio_in.value = LogicArray("Z" * 8)
     await ClockCycles(dut.clk, 5)  # wait until the wait state config is read
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 2)  # sync
 
     dut._log.info("test counter")
     for i in range(256):
-        assert dut.uo_out.value == dut.uio_out.value
+        assert dut.uo_out.value == dut.uio_out.value.integer
         assert dut.uo_out.value == i
         await ClockCycles(dut.clk, 1)  # wait until the wait state config is read
 
@@ -74,14 +78,25 @@ async def test_rom(dut):
     dut.rst_n.value = 0
     await enable_design(dut, 0)
 
-    dut._log.info("test ROM")
-    buf = bytearray(256)
-    for byte_idx in range(len(buf)):
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+
+    dut._log.info("Read ROM, addressing with ui_in (rst_n=1)")
+    rom_data = bytearray(256)
+    for byte_idx in range(len(rom_data)):
         dut.ui_in.value = byte_idx
         await ClockCycles(dut.clk, 1)
-        buf[byte_idx] = dut.uo_out.value.integer
+        rom_data[byte_idx] = dut.uo_out.value.integer
 
-    text = buf[32:128].rstrip(b"\0").decode("ascii")
+    dut._log.info(f"Read ROM, addressing sequentially (rst_n=0)")
+    dut.rst_n.value = 0
+    rom_data_seq = bytearray(256)
+    for byte_idx in range(len(rom_data_seq)):
+        await ClockCycles(dut.clk, 1)
+        rom_data_seq[byte_idx] = dut.uo_out.value.integer
+
+    text = rom_data[32:128].rstrip(b"\0").decode("ascii")
     items = {}
     for line in text.split("\n"):
         if len(line) == 0:
@@ -89,18 +104,22 @@ async def test_rom(dut):
         key, value = line.split("=", 2)
         items[key] = value
 
-    dut._log.info(f"ROM start bytes: {binascii.hexlify(buf[:32], ' ').decode('ascii')}")
+    dut._log.info(
+        f"ROM start bytes: {binascii.hexlify(rom_data[:32], ' ').decode('ascii')}"
+    )
     dut._log.info(f"ROM text data: {items}")
 
     assert "shuttle" in items
     assert "repo" in items
     assert "commit" in items
 
+    assert rom_data == rom_data_seq, "ROM contents differ when addressing sequentially"
+
     assert items["repo"] == os.environ.get("EXPECTED_REPO")
     assert re.match("^[0-9a-f]{8}$", items["commit"]) != None
 
-    magic = buf[248:252]
-    assert magic == b"TT\xFA\xBB"
+    magic = rom_data[248:252]
+    assert magic == b"TT\xfa\xbb"
 
-    crc32 = int.from_bytes(buf[252:256], "little")
-    assert crc32 == binascii.crc32(buf[0:252])
+    crc32 = int.from_bytes(rom_data[252:256], "little")
+    assert crc32 == binascii.crc32(rom_data[0:252])
